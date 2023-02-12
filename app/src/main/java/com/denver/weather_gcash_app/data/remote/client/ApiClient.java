ed428376a01d54b333bc8a9bf28d0e44c4b2d455
Denver <gregoriomarkdenver01@gmail.com>
2023-02-13 12:13:26 +1345

    Disable public key in source code

--- app/src/main/java/com/denver/weather_gcash_app/data/remote/client/ApiClient.java
+++ app/src/main/java/com/denver/weather_gcash_app/data/remote/client/ApiClient.java
@@ -2,5 +2,5 @@ package com.denver.weather_gcash_app.data.remote.client;
 
 public class ApiClient {
     public static final String BASE_URL = "https://api.openweathermap.org/data/2.5/";
-    public static final String API_KEY = "8d9f418904b3834e5f8f7cc3689f0c5c";
+    public static final String API_KEY = "";
 }
